8e5fc00df70433c9eb7d45c3aa6e2fa6f5e28c22
rokussadee <rokus.sadee@student.ehb.be>
2023-08-12 14:59:25 +0200

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8888'
+  baseURL: 'https://discjunky-api-web2ss.onrender.com'
 })
 
 export default instance
